e35635cc6e0625499e86fffdd9ac7271cd96c701
W10 <W10@DESKTOP-BV4GQ4F>
2018-11-13 20:07:19 +0100

    index bean hecho

--- src/java/es/uma/a6/beans/SessionBean.java
+++ src/java/es/uma/a6/beans/SessionBean.java
@@ -5,9 +5,14 @@
  */
 package es.uma.a6.beans;
 
+import WebService.Campaña;
+import WebService.Modulo;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
 
 /**
  *
@@ -16,11 +21,38 @@ import java.io.Serializable;
 @Named(value = "sessionBean")
 @SessionScoped
 public class SessionBean implements Serializable {
+    private List<Campaña> listaCampanyas;
+    private List<Modulo> listaModulos;
 
     /**
      * Creates a new instance of SessionBean
      */
     public SessionBean() {
     }
+
+    @PostConstruct
+    public void init(){
+        listaCampanyas= new ArrayList<>();
+        listaModulos= new ArrayList<>();
+    }
+    public List<Campaña> getListaCampanyas() {
+        return listaCampanyas;
+    }
+
+    public void setListaCampanyas(List<Campaña> listaCampanyas) {
+        this.listaCampanyas = listaCampanyas;
+    }
+
+    public List<Modulo> getListaModulos() {
+        return listaModulos;
+    }
+
+    public void setListaModulos(List<Modulo> listaModulos) {
+        this.listaModulos = listaModulos;
+    }
+    
+    
+    
+    
     
 }
