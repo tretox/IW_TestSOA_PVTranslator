234cd6310fce01a66680fb0c73cf9e457d0fca54
Carlos <Carlos@192.168.1.45>
2018-11-13 22:24:55 +0100

    Falta arreglar radio button

--- src/java/es/uma/a6/beans/CampanyaBean.java
+++ src/java/es/uma/a6/beans/CampanyaBean.java
@@ -5,21 +5,40 @@
  */
 package es.uma.a6.beans;
 
+import WebService.Campaña;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 
 /**
  *
  * @author Carlos
  */
 @Named(value = "campanyaBean")
-@Dependent
+@RequestScoped
 public class CampanyaBean {
+    
+    @Inject
+    private SessionBean sesion;
+    
+    private List<Campaña> campanyas;
 
     /**
      * Creates a new instance of CampanyaBean
      */
     public CampanyaBean() {
+    }
+    
+    @PostConstruct
+    public void init(){
+       campanyas=sesion.getListaCampanyas();
+    }
+
+    public List<Campaña> getCampanyas() {
+        return campanyas;
     }
     
     public String doAtras(){
--- src/java/es/uma/a6/beans/ModuloBean.java
+++ src/java/es/uma/a6/beans/ModuloBean.java
@@ -5,8 +5,12 @@
  */
 package es.uma.a6.beans;
 
+import WebService.Modulo;
+import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 
 /**
  *
@@ -15,13 +19,31 @@ import javax.enterprise.context.RequestScoped;
 @Named(value = "moduloBean")
 @RequestScoped
 public class ModuloBean {
-
+    
+    @Inject
+    private SessionBean sesion;
+    
+    private List<Modulo> modulos; 
+    
     /**
      * Creates a new instance of ModuloBean
      */
     public ModuloBean() {
     }
     
+    @PostConstruct
+    public void init(){
+        modulos = sesion.getListaModulos();
+    }
+
+    public List<Modulo> getModulos() {
+        return modulos;
+    }
+    
+    
+    public String doAtras(){
+        return "index.xhtml";
+    }
     
     
 }
